2e158256e173143ade4595aaa5c52faea73c1aa4
McCabe Northup <mccabenorthup@gmail.com>
2020-02-13 21:39:58 -0700

    added models directory for schema

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// comparing this to seeds.js
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises : [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter a type of exercise"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter the name of exercise"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter the duration of exercise"
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }
+    ]
+});
+const Workout =mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
